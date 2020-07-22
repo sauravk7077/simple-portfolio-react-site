868abe9aec755e23084118da2dbfe186e3834e3d
sauravk7077 <sauravk7077@gmail.com>
2020-07-22 09:45:47 +0530

    First Commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+function NavBar(props) {
+    return (
+        <div className="flexBox">
+            <h1>Portfolio Site</h1>
+            <div>
+                <div>
+                    <a href="">Home</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
