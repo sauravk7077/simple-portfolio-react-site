2775d261aa170f299b809eb004ecbad968e5b27a
sauravk7077 <sauravk7077@gmail.com>
2020-09-07 08:36:49 +0530

    changed data source

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,25 @@
 import React from "react";
-import {contents} from "./Data/aboutData";
 
 class About extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            data:[]
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            let res = await fetch('https://raw.githubusercontent.com/sauravk7077/sauravk7077.github.io/master/aboutData.json');
+            let contents = await res.json();
+            this.setState({data: contents.contents})
+        } catch (error) {
+            console.log("Some error occured")
+        }
+    }
+
     render() {
-        const data = contents.map(e=>(
+        const data = this.state.data.map(e=>(
             <div className="flex column">
                 <div className="heading">
                     <h2>{e.heading}</h2>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {contents} from "./Data/portfolioData";
 import Card from "./InnerComponents/Card";
 
 class Portfolio extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            data:[]
+        }
+    }
 
+    async componentDidMount() {
+        try {
+            let res = await fetch('https://raw.githubusercontent.com/sauravk7077/sauravk7077.github.io/master/portfolioData.json');
+            let contents = await res.json();
+            this.setState({data: contents.contents})
+        } catch (error) {
+            console.log("Some error occured")
+        }
+    }
     render() {
-        const c = contents.map(e=> (
+        const c = this.state.data.map(e=> (
             <Card key={e.id} title={e.title} src={`./images/${e.img}`} detail={e.detail} git={e.git}/>
         ))
         return (
